9d5fa463b46a5ced68ae0efb8d013f8b84ab651a
Rushikesh Harne <161678001+RushikeshHarne@users.noreply.github.com>
2026-07-06 13:31:41 +0530

    Format app_port variable definition for clarity

--- infra/modules/network/variables.tf
+++ infra/modules/network/variables.tf
@@ -3,4 +3,7 @@ variable "vpc_cidr"             { type = string }
 variable "public_subnet_cidrs"  { type = list(string) }
 variable "private_subnet_cidrs" { type = list(string) }
 variable "availability_zones"   { type = list(string) }
-variable "app_port"             { type = number; default = 80 }
+variable "app_port"             { 
+  type = number
+  default = 80 
+}
