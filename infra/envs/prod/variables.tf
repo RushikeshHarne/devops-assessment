0f655480966d937b65ebf08628c3a4f58e69ee6d
Rushikesh Harne <161678001+RushikeshHarne@users.noreply.github.com>
2026-07-06 13:39:11 +0530

    Update variables.tf

--- infra/envs/prod/variables.tf
+++ infra/envs/prod/variables.tf
@@ -1,12 +1,21 @@
-variable "env"        { type = string; default = "prod" }
-variable "aws_region" { type = string; default = "ap-south-1" }
+variable "env"        { 
+  type = string
+  default = "prod"
+}
+variable "aws_region" { 
+  type = string
+  default = "ap-south-1" 
+}
 
 variable "vpc_cidr"             { type = string }
 variable "public_subnet_cidrs"  { type = list(string) }
 variable "private_subnet_cidrs" { type = list(string) }
 variable "availability_zones"   { type = list(string) }
 
-variable "app_port"         { type = number; default = 80 }
+variable "app_port"         {
+  type = number
+  default = 80
+}
 variable "container_image"  { type = string }
 
 variable "ecs_task_cpu"     { type = number }
@@ -15,7 +24,10 @@ variable "ecs_desired_count"{ type = number }
 
 variable "db_name"     { type = string }
 variable "db_username" { type = string }
-variable "db_password" { type = string; sensitive = true }
+variable "db_password" { 
+  type = string
+  sensitive = true 
+}
 
 variable "rds_instance_class"          { type = string }
 variable "rds_allocated_storage"       { type = number }
