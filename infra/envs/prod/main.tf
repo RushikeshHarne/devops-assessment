965a634d6c4013fc2c8fc1ca2d314f9a969d5989
Rushikesh Harne <161678001+RushikeshHarne@users.noreply.github.com>
2026-07-06 13:40:27 +0530

    Disable S3 backend in Terraform configuration
    Comment out the S3 backend configuration in main.tf

--- infra/envs/prod/main.tf
+++ infra/envs/prod/main.tf
@@ -8,13 +8,13 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "my-terraform-state-prod"
-    key            = "prod/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
+#  backend "s3" {
+ #   bucket         = "my-terraform-state-prod"
+ #   key            = "prod/terraform.tfstate"
+#    region         = "ap-south-1"
+#    dynamodb_table = "terraform-locks"
+#    encrypt        = true
+#  }
 }
 
 provider "aws" {
